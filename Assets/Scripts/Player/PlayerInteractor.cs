c3374d1295f96d9bc37f2b8062bfc5fbddf481cd
DavFlo05 <davflor155@gmail.com>
2026-03-10 20:02:31 -0400

    Revert "Light Optimization & Drop Sound"
    This reverts commit c6e07773ef2e09eb97cd3bc720dbcfb751f48a7a.

--- Assets/Scripts/Player/PlayerInteractor.cs
+++ Assets/Scripts/Player/PlayerInteractor.cs
@@ -10,7 +10,6 @@ public class PlayerInteractor : MonoBehaviour
     public AngelEnemy[] angels;
 
     public AudioClip pickupSound;
-    public AudioClip dropSound;
     public AudioClip angelWakeSound;
 
     [Range(0f, 1f)] public float volume = 1f;
@@ -79,11 +78,6 @@ public class PlayerInteractor : MonoBehaviour
 
             if (point && heldItems.Count > 0)
             {
-                if (dropSound != null)
-                {
-                    AudioSource.PlayClipAtPoint(dropSound, point.transform.position, volume);
-                }
-
                 GameObject itemToDrop = heldItems[0];
                 heldItems.RemoveAt(0);
                 point.ReceiveItem(itemToDrop);
